9be4ef0cd34ae42c1a23a247d6869d78f0cc138a
M-a-t-e-o <874162@unizar.es>
2026-03-15 21:20:48 +0100

    add index/fix merge

--- backend/index.js
+++ backend/index.js
@@ -19,3 +19,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
